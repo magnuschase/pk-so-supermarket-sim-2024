902092e3b73de03c3b5baa6387cc9d0dc9ce8861
Jakub Kapała <jakub@kapala.xyz>
2024-12-14 20:39:56 +0100

    feat: add empty `client.c` file

---
+++ client.c
@@ -0,0 +1,15 @@
+#include "semaphores.h"
+
+/*
+
+Systemy operacyjne - projekt 2024/2025 - Supermarket (temat 10)
+Imię i nazwisko: Jakub Kapała
+Numer albumu: 151885
+Kierunek: Informatyka, niestacjonarne
+
+*/ 
+
+int main()
+{
+	return 0;
+}
